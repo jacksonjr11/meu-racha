4f1eb8e56cc7e5535dbf87e8498d1bc5af072c61
Jackson Augusto <jackson.augusto.1571@gmail.com>
2025-06-28 01:14:50 -0300

    Created custom decorator to handle with public routers

--- identity/src/application/auth/guard/auth.guard.ts
+++ identity/src/application/auth/guard/auth.guard.ts
@@ -1,5 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { AuthGuard as Guard } from '@nestjs/passport';
+import { IS_PUBLIC_KEY } from 'src/application/common/decorator/public';
 
 @Injectable()
-export class AuthGuard extends Guard('jwt') {}
+export class AuthGuard extends Guard('jwt') {
+  constructor(private reflector: Reflector) {
+    super();
+  }
+
+  canActivate(context: ExecutionContext) {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (isPublic) {
+      return true;
+    }
+    return super.canActivate(context);
+  }
+}
